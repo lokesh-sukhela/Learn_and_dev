866ef3d273a1a8c1dd5f3e96a7c4fb1f91f2f690
Kishore L <kishore.l@jmangroup.com>
2023-09-11 10:31:29 +0530

    uploaded Successfully

---
+++ react_frontend/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
+import Mainfile from './components/mainfile';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
+import Sidebar from './components/admin2';
+
+function App() {
+ 
+    return (
+      <>
+    
+      <Router>
+      <Routes>
+        <Route path="/" element={<Mainfile/>}></Route>
+     <Route path="/admin" element={<Sidebar/>}></Route>
+      </Routes>
+    </Router>
+      </>
+  );
+   
+    
+  
+}
+
+export default App;
