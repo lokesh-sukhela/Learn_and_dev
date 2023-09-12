64d41ff308c1abdeab50cd8d2ac649b569a87295
YSG-DS19 <yash10gavali@gmail.com>
2023-09-12 19:50:19 +0530

    Updated with material ui

--- react_frontend/src/App.js
+++ react_frontend/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState } from 'react';
 import Sidebar from './components/admin/AdminTrainingDetails';
 import './style.css';
 import SignUpAndSignInCombine from './components/login/SignUpAndSignInCombine';
+import TrainingTable from './components/admintrainingatble/AdminTrainingTable';
 
 
 function App() {
@@ -17,6 +17,7 @@ function App() {
       <Routes>
         <Route path="/" element={<SignUpAndSignInCombine/>}></Route>
      <Route path="/admin" element={<Sidebar/>}></Route>
+     <Route path="/adminTrainingTable" element={<TrainingTable/>}></Route>
       </Routes>
     </Router>
       </>
