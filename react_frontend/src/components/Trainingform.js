2d9595bce0ab048ffd48eee9794ae9ba06d9fe1e
lokesh-sukhela <lokeshsukhela05045@gmail.com>
2023-09-13 14:59:51 +0530

    COMMIT

--- react_frontend/src/components/Trainingform.js
+++ react_frontend/src/components/Trainingform.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import {
   TextField,
   Button,
@@ -11,54 +11,48 @@ import {
   Select,
   MenuItem,
 } from '@mui/material';
-const TrainingForm = ({ addTraining }) => {
-  const [formData, setFormData] = useState({
-    title: '',
-    skillType: '',
-    skillCategory: '',
-    startDateAndTime: '',
-    endDateAndTime: '',
-    description: '',
+import { ForkRight } from '@mui/icons-material';
+
+const TrainingForm = ({ isEditing, editedTraining, onSave, onCancel }) => {
+  const [formData, setFormData] = useState(() => {
+    // Initialize with editedTraining data if editing, or with default values if not
+    return isEditing
+      ? editedTraining
+      : {
+          title: '',
+          skillType: '',
+          skillCategory: '',
+          startDateAndTime: '',
+          endDateAndTime: '',
+          description: '',
+          count: 0, // Add any other default values you may have
+          mode: '', // Add any other default values you may have
+          Link: 'Chennai', // Add any other default values you may have
+        };
   });
- 
 
-  const [trainings, setTrainings] = useState([]);
+  const [isVirtualSelected, setIsVirtualSelected] = useState(
+    // Initialize isVirtualSelected based on formData.mode when editing
+    isEditing && formData.mode === 'virtual'
+  );
+
   const navigate = useNavigate();
-
-  useEffect(() => {
-    const storedTrainings = JSON.parse(localStorage.getItem('trainings')) || [];
-    setTrainings(storedTrainings);
-  }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const newTraining = {
-      id: Date.now(),
+      id: isEditing ? formData.id : Date.now(), // Use existing ID if editing, generate a new one if not
       ...formData,
     };
     navigate('/trainingtable');
-    const updatedTrainings = [...trainings, newTraining];
-    setTrainings(updatedTrainings);
-    localStorage.setItem('trainings', JSON.stringify(updatedTrainings));
-    setFormData({
-      title: '',
-      skillType: '',
-      skillCategory: '',
-      startDateAndTime: '',
-      endDateAndTime: '',
-      description: '',
-    });
-    function addTraining (newTraining) {
-        
-        const updatedTrainings = [...trainings, newTraining];
-        setTrainings(updatedTrainings);
-        localStorage.setItem('trainings', JSON.stringify(updatedTrainings));
-        
-      };
+    onSave(newTraining);
   };
 
   const handleChange = (e) => {
     const { name, value } = e.target;
+    if (name === 'mode') {
+      setIsVirtualSelected(value === 'virtual');
+    }
     setFormData({ ...formData, [name]: value });
   };
 
@@ -89,9 +83,25 @@ const TrainingForm = ({ addTraining }) => {
       }
     }
   };
+
+  // ... rest of the component remains the same
+
+
+
+  const handleSave = () => {
+    if (isEditing) {
+      // Update the existing training
+      const updatedTraining = { ...editedTraining, /* update fields from form inputs */ };
+      onSave(updatedTraining);
+    } else {
+      // Create a new training
+      const newTraining = { /* create a new training object from form inputs */ };
+      onSave(newTraining);
+    }
+  };
   const handleChangestartdate = (event) => {
     const { name, value } = event.target;
-    
+
     // Check if the entered date is at least 7 days from the current date
     const currentDate = new Date();
     const selectedDate = new Date(value);
@@ -113,126 +123,155 @@ const TrainingForm = ({ addTraining }) => {
   };
   return (
     <Container maxWidth="md">
-    <Typography variant="h4" gutterBottom>
-      Add New Training
-    </Typography>
-    <form onSubmit={handleSubmit}>
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <TextField
-            fullWidth
-            label="Title"
-            name="title"
-            value={formData.title}
-            onChange={handleChange}
-            required
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <FormControl fullWidth required>
-            <InputLabel>Skill Type</InputLabel>
-            <Select
-              name="skillType"
-              value={formData.skillType}
-              onChange={handleChange}
-            >
-              <MenuItem value="">Select Skill Type</MenuItem>
-              <MenuItem value="Full Stack Developer">Full Stack Developer</MenuItem>
-              <MenuItem value="Data Engineer">Data Engineer</MenuItem>
-              <MenuItem value="Cloud Manager">Cloud Manager</MenuItem>
-            </Select>
-          </FormControl>
-        </Grid>
-        
-        <Grid item xs={12} sm={6}>
-          <FormControl fullWidth required>
-            <InputLabel>Skill Category</InputLabel>
-            <Select 
-              name="skillCategory"
-              value={formData.skillCategory}
-              onChange={handleChange}
-            >
-              <MenuItem value="">Select Skill category</MenuItem>
-              <MenuItem value="Physical">Physical</MenuItem>
-              <MenuItem value="virtual">virtual</MenuItem>
-              <MenuItem value="Azure">Azure </MenuItem>
-              <MenuItem value="Frontend">Frontend</MenuItem>
-              <MenuItem value="Backend">Backend</MenuItem>
+      <Typography variant="h4" gutterBottom>
+        {isEditing ? 'Edit Training' : 'Add New Training'}
+      </Typography>
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-end' }}>
+        <form onSubmit={handleSubmit} >
+          <Grid container spacing={2} >
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                label="Title"
+                name="title"
+                value={formData.title}
+                onChange={handleChange}
+                required
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <FormControl fullWidth required>
+                <InputLabel>Skill Type</InputLabel>
+                <Select
+                  name="skillType"
+                  value={formData.skillType}
+                  onChange={handleChange}
+                >
+                  <MenuItem value="">Select Skill Type</MenuItem>
+                  <MenuItem value="Full Stack Developer">Full Stack Developer</MenuItem>
+                  <MenuItem value="Data Engineer">Data Engineer</MenuItem>
+                  <MenuItem value="Cloud Manager">Cloud Manager</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
 
-            </Select>
-          </FormControl>
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-           fullWidth
-           label="Start Date and Time"
-           type="datetime-local"
-           name="startDateAndTime"
-           value={formData.startDateAndTime}
-           onChange={handleChangestartdate}
-           required
-           InputLabelProps={{ shrink: true }}
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            fullWidth
-            label="End Date and Time"
-            type="datetime-local"
-            name="endDateAndTime"
-            value={formData.endDateAndTime}
-            onChange={handleChangeenddate}
-            required
-            InputProps={{ placeholder: '' }}
-            InputLabelProps={{ shrink: true }}
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            fullWidth
-            label="Max count of registrations"
-            type="number"
-            name="count"
-            value={formData.count}
-            onChange={handleChange}
-            required
-            // disabled
-            InputProps={{ placeholder: '' }}
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-        <FormControl fullWidth required>
-            <InputLabel>Mode</InputLabel>
-            <Select 
-              name="mode"
-              value={formData.mode}
-              onChange={handleChange}
-            >
-              <MenuItem value="">Select Model of training</MenuItem>
-              <MenuItem value="Python">Physical</MenuItem>
-              <MenuItem value="virtual">virtual</MenuItem>
-            </Select>
-          </FormControl>
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            fullWidth
-            label="Description"
-            multiline
-            rows={4}
-            name="description"
-            value={formData.description}
-            onChange={handleChange}
-            required
-          />
-        </Grid>
-      </Grid>
-      <Button type="submit" variant="contained" color="primary">
-        Add Training
-      </Button>
-    </form>
-  </Container>
-    
+            <Grid item xs={12} sm={6}>
+              <FormControl fullWidth required>
+                <InputLabel>Skill Category</InputLabel>
+                <Select
+                  name="skillCategory"
+                  value={formData.skillCategory}
+                  onChange={handleChange}
+                >
+                  <MenuItem value="">Select Skill category</MenuItem>
+                  <MenuItem value="Python">Python</MenuItem>
+                  <MenuItem value="Azure">Azure </MenuItem>
+                  <MenuItem value="Frontend">Frontend</MenuItem>
+                  <MenuItem value="Backend">Backend</MenuItem>
+
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                fullWidth
+                label="Start Date and Time"
+                type="datetime-local"
+                name="startDateAndTime"
+                value={formData.startDateAndTime}
+                onChange={handleChangestartdate}
+                required
+                InputLabelProps={{ shrink: true }}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                fullWidth
+                label="End Date and Time"
+                type="datetime-local"
+                name="endDateAndTime"
+                value={formData.endDateAndTime}
+                onChange={handleChangeenddate}
+                required
+                InputProps={{ placeholder: '' }}
+                InputLabelProps={{ shrink: true }}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                fullWidth
+                label="Max count of registrations"
+                type="number"
+                name="count"
+                value={formData.count}
+                onChange={handleChange}
+                required
+                // disabled
+                InputProps={{ placeholder: '' }}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <FormControl fullWidth required className='trainingmode'>
+                <InputLabel>Mode</InputLabel>
+                <Select
+                  name="mode"
+                  value={formData.mode}
+                  onChange={(e) => {
+                    handleChange(e);
+                    // setIsVirtualSelected(e.target.value === 'virtual');
+                  }}
+                >
+                  <MenuItem value="Select Mode of training"></MenuItem>
+                  <MenuItem value="Physical">Physical</MenuItem>
+                  <MenuItem value="virtual">Virtual</MenuItem>
+                </Select>
+              </FormControl>
+
+              {isVirtualSelected && (
+                <FormControl fullWidth required>
+
+                  <TextField
+                    name="meetingLink"
+                    label="Meeting Link"
+                    value={formData.meetingLink}
+                    onChange={handleChange}
+                    InputLabelProps={{
+                      shrink: Boolean(formData.meetingLink),
+                    }}
+                  />
+                </FormControl>
+              )}
+
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                label="Description"
+                multiline
+                rows={4}
+                name="description"
+                value={formData.description}
+                onChange={handleChange}
+                required
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            className='addtrainingbutton'
+            style={{
+              marginTop: '16px',
+              marginLeft: "71.6%"// Add some space between the form and the button
+            }}
+          >
+            Add Training
+          </Button>
+        </form>
+      </div>
+    </Container>
+
   );
 };
 
