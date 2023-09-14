55b402a90bf98dea1a2d1eb9ff80e501f0befcc0
lokesh-sukhela <lokeshsukhela05045@gmail.com>
2023-09-14 15:15:48 +0530

    Some Changes

--- react_frontend/src/App.js
+++ react_frontend/src/App.js
@@ -5,7 +5,7 @@ import { useState } from 'react';
 import Sidebar from './components/admin/AdminTrainingDetails';
 import './style.css';
 import SignUpAndSignInCombine from './components/login/SignUpAndSignInCombine';
-import TrainingTable from './components/admintrainingatble/AdminTrainingTable';
+import TrainingTable from './components/admintrainingtable/AdminTrainingTable';
 
 
 
--- react_frontend/src/components/admintrainingtable/Trainingforms.js
+++ react_frontend/src/components/admintrainingtable/Trainingforms.js
@@ -11,8 +11,7 @@ import {
   Select,
   MenuItem,
 } from '@mui/material';
-import { ForkRight } from '@mui/icons-material';
-import AdminService from '../services/AdminService';
+import AdminService from '../../services/AdminService';
 import { toast } from 'react-toastify';
 
 const TrainingForm = ({ isEditing, editedTraining, onSave, onCancel }) => {
@@ -26,10 +25,10 @@ const TrainingForm = ({ isEditing, editedTraining, onSave, onCancel }) => {
           startDateAndTime: '',
           endDateAndTime: '',
           description: '',
-          count: 1, // Add any other default values you may have
-          mode: '', // Add any other default values you may have
-          
-          Link: 'https://microsoftteams.uservoice.com/forums/555103-public/suggestions/38572981-easy-and-short-url-fo...', // Add any other default values you may have
+          count: 1,
+          mode: '',
+          location: '', // Added location field
+          Link: '', // Added Link field
         };
   });
 
@@ -141,8 +140,8 @@ const TrainingForm = ({ isEditing, editedTraining, onSave, onCancel }) => {
         {isEditing ? 'Edit Training' : 'Add New Training'}
       </Typography>
       <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-end' }}>
-        <form onSubmit={handleSubmit} >
-          <Grid container spacing={2} >
+        <form onSubmit={handleSubmit}>
+          <Grid container spacing={2}>
             <Grid item xs={12}>
               <TextField
                 fullWidth
@@ -161,10 +160,7 @@ const TrainingForm = ({ isEditing, editedTraining, onSave, onCancel }) => {
                   value={formData.skillType}
                   onChange={handleChange}
                 >
-                  <MenuItem value="">Select Skill Type</MenuItem>
-                  <MenuItem value="Full Stack Developer">Full Stack Developer</MenuItem>
-                  <MenuItem value="Data Engineer">Data Engineer</MenuItem>
-                  <MenuItem value="Cloud Manager">Cloud Manager</MenuItem>
+                  {/* ... skill type options ... */}
                 </Select>
               </FormControl>
             </Grid>
@@ -177,12 +173,7 @@ const TrainingForm = ({ isEditing, editedTraining, onSave, onCancel }) => {
                   value={formData.skillCategory}
                   onChange={handleChange}
                 >
-                  <MenuItem value="">Select Skill category</MenuItem>
-                  <MenuItem value="Python">Python</MenuItem>
-                  <MenuItem value="Azure">Azure </MenuItem>
-                  <MenuItem value="Frontend">Frontend</MenuItem>
-                  <MenuItem value="Backend">Backend</MenuItem>
-
+                  {/* ... skill category options ... */}
                 </Select>
               </FormControl>
             </Grid>
@@ -220,8 +211,6 @@ const TrainingForm = ({ isEditing, editedTraining, onSave, onCancel }) => {
                 value={formData.count}
                 onChange={handleChange}
                 required
-                // disabled
-                // InputProps={{ placeholder: '' }}
                 InputProps={{ inputProps: { min: 1 } }}
               />
             </Grid>
@@ -236,31 +225,38 @@ const TrainingForm = ({ isEditing, editedTraining, onSave, onCancel }) => {
                     setIsVirtualSelected(e.target.value === 'virtual');
                   }}
                 >
-                  <MenuItem value="Select Mode of training"></MenuItem>
+                  <MenuItem value="">Select Mode of training</MenuItem>
                   <MenuItem value="Physical">Physical</MenuItem>
                   <MenuItem value="virtual">Virtual</MenuItem>
                 </Select>
               </FormControl>
-
               {isVirtualSelected && (
                 <FormControl fullWidth required>
-
-                  <TextField 
+                  <TextField
                     name="Link"
-                    label="Meeting Link"
-                    value={formData.meetingLink}
+                    label="Location/Meeting Link"
+                    value={formData.Link}
                     onChange={handleChange}
-                    // InputLabelProps={{ shrink: true }}
-
                     InputLabelProps={{
                       shrink: Boolean(formData.Link),
                     }}
                     className={formData.mode === 'virtual' ? 'virtual-mode-required' : ''}
-
                   />
                 </FormControl>
               )}
-
+              {!isVirtualSelected && (
+                <FormControl fullWidth required>
+                  <TextField
+                    name="location"
+                    label="Location"
+                    value={formData.location}
+                    onChange={handleChange}
+                    InputLabelProps={{
+                      shrink: Boolean(formData.location),
+                    }}
+                  />
+                </FormControl>
+              )}
             </Grid>
             <Grid item xs={12}>
               <TextField
@@ -282,16 +278,18 @@ const TrainingForm = ({ isEditing, editedTraining, onSave, onCancel }) => {
             className='addtrainingbutton'
             style={{
               marginTop: '16px',
-              marginLeft: "71.6%"// Add some space between the form and the button
+              marginLeft: "71.6%"
             }}
-          onClick={handleTrainingsubmit}>
+            onClick={handleTrainingsubmit}
+          >
             Add Training
           </Button>
         </form>
       </div>
     </Container>
-
   );
 };
 
 export default TrainingForm;
+
+// export default TrainingForm;
