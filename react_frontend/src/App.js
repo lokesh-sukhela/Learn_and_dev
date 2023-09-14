5c9ccd934e250ce6f5a2de8f88c264f9e59e7de1
lokesh-sukhela <lokeshsukhela05045@gmail.com>
2023-09-14 19:34:13 +0530

    added

--- react_frontend/src/App.js
+++ react_frontend/src/App.js
@@ -5,7 +5,7 @@ import { useState } from 'react';
 import Sidebar from './components/admin/AdminTrainingDetails';
 import './style.css';
 import SignUpAndSignInCombine from './components/login/SignUpAndSignInCombine';
-import TrainingTable from './components/admintrainingatble/AdminTrainingTable';
+import TrainingTable from './components/admintrainingtable/AdminTrainingTable';
 import UserTrainingTable from './components/UserTrainingTable/UserTrainingTable';
 
 
@@ -18,9 +18,9 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<SignUpAndSignInCombine />}></Route>
-          <Route path="/admin" element={<Sidebar />}></Route>
+          {/* <Route path="/admin" element={<Sidebar />}></Route> */}
           <Route path="/adminTrainingTable" element={<TrainingTable />}></Route>
-          <Route path="/userTrainingTable" element={<UserTrainingTableTrainingTable />}></Route>
+          <Route path="/userTrainingTable" element={<UserTrainingTable />}></Route>
         </Routes>
       </Router>
     </>
